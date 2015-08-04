4892eeb09073bd9a25f9c18461bd96411e0bce7c
morishitter <morishitter@gmail.com>
2015-08-04 15:03:45 +0900

    test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,10 @@
+var fs = require('fs')
 var test = require('tape')
 var cssfmt = require('..')
 
-test('description', function (t) {
-    t.end()
+test('fmt', function (t) {
+  var input = 'test/input.css'
+  var expected = fs.readFileSync('test/output.css', 'utf-8')
+  t.equal(cssfmt(input).toString(), expected)
+  t.end()
 })
