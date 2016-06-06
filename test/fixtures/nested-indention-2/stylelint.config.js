70f1f8dcc31b34502368b6ec4caf8b83c9d1a443
seka <s.seka134704@gmail.com>
2016-06-06 10:01:34 +0900

    Fix nested indention (#141)
    * add a test for nested indention
    
    * fix a bug #137
    
    fix a format rule for block-opening-brace-newline-before
    
    * add a test for nested indention2
    
    * fix a format rule for selector-list-comma-newline-before
    
    * tweak

---
+++ test/fixtures/nested-indention-2/stylelint.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    rules: {
+        'selector-list-comma-newline-before': 'always',
+        'indentation': 4
+    }
+};
---
+++ test/fixtures/nested-indention/stylelint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    rules: {
+        'block-opening-brace-newline-before': 'always',
+        'block-opening-brace-newline-after': 'always',
+        'indentation': 4
+    }
+};
